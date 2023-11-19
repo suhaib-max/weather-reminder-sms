74a99383d47c9d7eb4c363663f6847ad5696d1cd
suhaib <suhaibvbm@gmail.com>
2023-11-19 13:27:34 +0530

    calling openweather api

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "57235ac50346d7d3c57810d992ca5b5a"
+
+weather_parameter = {
+    "lat": 11.127693,
+    "lon": 76.044945,
+    "appid": api_key,
+}
+
+
+
+
+
+
+
+
